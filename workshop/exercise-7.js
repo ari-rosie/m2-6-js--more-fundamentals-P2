45fb71e1aa9957d9d55e0d00c658a6f72227a6ce
ari-rosie <62956748+ari-rosie@users.noreply.github.com>
2020-06-23 19:45:12 -0400

    Exercice-7

--- workshop/exercise-7.js
+++ workshop/exercise-7.js
@@ -10,7 +10,15 @@ let foodPairings = {
 
 // createRecommendations function
 
-// printRecommendations function
+const createRecommendations = (obj) => {
+  let pairs = Object.entries(obj);
+  return pairs;
+}
+const printRecommendations = (arr) => {
+  arr.forEach(pair => {
+    console.log(`With ${pair[0]}, it is best to have ${pair[1]}.`);
+  });
+}
 
 // function call (done)
 printRecommendations(createRecommendations(foodPairings));
