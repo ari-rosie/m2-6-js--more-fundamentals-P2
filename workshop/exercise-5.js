2a9322496347483754dede9c0abb16b5d94d7453
ari-rosie <62956748+ari-rosie@users.noreply.github.com>
2020-06-23 18:57:53 -0400

    Exercice-5

--- workshop/exercise-5.js
+++ workshop/exercise-5.js
@@ -8,8 +8,11 @@ const foodPairings = {
   strawberries: 'cream',
 };
 
-const getValue = () => {
-  // do something
+const getValue = (obj, key) => {
+  if (Object.keys(obj).includes(key)) 
+    return obj[key];
+  else  
+    return 'not available';  
 };
 
 expect(getValue(foodPairings, 'burgers'), 'beer');
