c4d964d2d19a14a8d7bc022ff15f0f41de24a6c3
ari-rosie <62956748+ari-rosie@users.noreply.github.com>
2020-06-23 14:59:51 -0400

    Exercice-2

--- workshop/exercise-2.js
+++ workshop/exercise-2.js
@@ -3,3 +3,12 @@
 
 const lateComers = ['Felicia', 'Charles', 'Terry'];
 const earlyBirds = ['Ellen', 'Jerry', 'Jasmine'];
+
+const printGuestOrder = (arr1, arr2) => {
+    arr =arr1.concat(arr2);
+    arr.forEach(guest => {
+        console.log(`${arr.indexOf(guest) + 1}. ${guest}`);
+    })
+}
+
+printGuestOrder(earlyBirds, lateComers);
